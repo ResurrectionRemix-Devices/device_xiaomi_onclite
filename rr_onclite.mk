5b00a1e1a00e9a6d91d43db363c908264a064550
revanth-24 <crevanthtolety@gmail.com>
2020-08-17 04:13:16 +0000

    onclite: Update BUILD_FINGERPRINT from tissot

--- rr_onclite.mk
+++ rr_onclite.mk
@@ -32,9 +32,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
